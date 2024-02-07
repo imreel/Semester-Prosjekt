17c8ede2614a63b3ee145169700dff2cb8048e3c
Imre Lind <imreel@uia.no>
2024-02-07 15:42:21 +0100

    cant seem to get the create,edit,user too work

--- modules/httpConstants.mjs
+++ modules/httpConstants.mjs
@@ -1,37 +1,34 @@
 
 // The point of this class is increasing readability and maintainability of the rest of the code. 
-// It should be extended and refactord as needed.
+// It should be extended and refactored as needed.
 
-class HTTPCodes {
+class HttpCodes {
 
-    static SuccesfullRespons = {
+    static successfulResponse = {
         Ok: 200
     }
 
-    static ClientSideErrorRespons = {
+    static ClientSideErrorResponse = {
         BadRequest: 400,
         Unauthorized: 401,
         PaymentRequired: 402,
-        Forbidden: 404,
+        Forbidden: 403,
         NotFound: 404,
         MethodNotAllowed: 405,
         NotAcceptable: 406
     }
 
-    static ServerErrorRespons = {
-        InternalError: 500,
-        NotImplemented: 501,
+    static serverSideResponse = {
+        
     }
 
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-const HTTPMethods = {
-    POST: "POST",
+export const HTTPMethods = {
+    POST : "POST",
     GET: "GET",
     PUT: "PUT",
     PATCH: "PATCH",
     DELETE: "DELETE"
 }
-
-export { HTTPCodes, HTTPMethods };
+export default HttpCodes;
